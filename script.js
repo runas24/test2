f3cdce1ad32e9f92b6c1a89a3ba7ef64c22fb3e0
runas24 <166917824+runas24@users.noreply.github.com>
2024-04-19 12:07:22 +0500

    Update and rename node.js to script.js

---
+++ script.js
@@ -0,0 +1,24 @@
+function formatCurrency(input) {
+    var value = input.value.replace(/\D/g, '');
+    var formattedValue = new Intl.NumberFormat('ru-RU').format(value);
+    input.value = formattedValue;
+}
+
+document.getElementById("loanForm").addEventListener("submit", function(event) {
+    event.preventDefault();
+
+    document.querySelector('button[type="submit"]').classList.add("loading");
+
+    var desiredAmount = parseFloat(document.getElementById("desiredAmount").value.replace(/\D/g, ''));
+    var creditBurden = parseFloat(document.getElementById("creditBurden").value.replace(/\D/g, ''));
+    var pensionContributions = parseFloat(document.getElementById("pensionContributions").value.replace(/\D/g, ''));
+
+    // Рассчитываем максимальную сумму кредита
+    // Больший процент от пенсионных отчислений увеличивает максимальную сумму кредита
+    var maxLoanAmount = desiredAmount - creditBurden + (pensionContributions * 6 * 2);
+
+    setTimeout(function() {
+        document.querySelector('button[type="submit"]').classList.remove("loading");
+        document.getElementById("maxLoanAmount").innerText = "Максимальная сумма кредита: " + maxLoanAmount.toFixed(2).replace(/\B(?=(\d{3})+(?!\d))/g, " ") + " тенге";
+    }, 1000);
+});
